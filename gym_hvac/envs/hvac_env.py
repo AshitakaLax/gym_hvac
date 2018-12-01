c76d0f97e5827e4ec858758a40774362e88161be
Levi Balling <leviballing@gmail.com>
2018-12-01 10:16:52 -0700

    I still hav to test it out, but I think I have the environment setup

--- gym_hvac/envs/hvac_env.py
+++ gym_hvac/envs/hvac_env.py
@@ -17,9 +17,24 @@ from gym_hvac.models import HvacBuilding
 
 class HvacEnv(gym.Env):
 	metadata = {'render.modes': ['human']}
-	def __init__(self, hvacBuilding:HvacBuilding):
+	def __init__(self, hvacBuilding:HvacBuilding, outsideTemperature:float = 28.0):
+
 		self.__version__ = "0.1.0"
-		self.__HvacBuilding = hvacBuilding
+		self.OutsideTemperature = outsideTemperature
+		self.hvacBuilding = hvacBuilding
+		# step environment variables
+		# we are currently saying there are 2 options Cooling on/off
+		# if you want more than one action then you need to provide a spaces.Tuple
+		self.action_space = spaces.Discrete(2)
+		
+		self.observation = 0.0
+		self.step_count = 0
+		self.step_max = 3600
+		# the observation currnently the average cost per second
+		self.observation_space = spaces.Box(low=0, high= self.hvacBuilding.building_hvac.GetMaxCoolingPower(), dtype=np.float32)
+		self.reset()
+
+
 
 	def step(self, action): 
 		"""
@@ -50,26 +65,36 @@ class HvacEnv(gym.Env):
                  However, official evaluations of your agent are not allowed to
                  use this for learning.
         """
+		assert self.action_space.contains(action)
 		self._take_action(action)
+		self.hvacBuilding
+		self.observation = self.hvacBuilding.building_hvac.GetAverageWattsPerSecond()
+
 		#self.status = self.env.step()
-		# reward = self._get_reward()
+		reward = self._get_reward()
 		#ob = self.env.getState()
-		#episode_over = self.status != hfo_py.IN_GAME
-		#return ob, reward, episode_over, {}
+		done = False
+		if self.step_count >= self.step_max:
+			done = True
+		return self.observation, reward, done, {self.hvacBuilding.current_temperature, self.hvacBuilding.building_hvac.CoolingIsOn }
 
 	def reset(self):
-		pass
+		self.hvacBuilding.reset()
+		self.observation = 0.0
+
 	def render(self, mode='human', close=False):
 		pass
 		
 	def _take_action(self, action):
-		pass
+		# convert
+		if action == 0:
+			self.hvacBuilding.building_hvac.TurnCoolingOn()
+		if action == 1:
+			self.hvacBuilding.building_hvac.TurnCoolingOff()
+		
+		self.hvacBuilding.step(self.OutsideTemperature)
+		self.step_count = self.step_count + 1
 	
 	def _get_reward(self):
-		""" Reward is given for XY. """
-		# if self.status == FOOBAR:
-		# 	return 1
-		# elif self.status == ABC:
-		# 	return self.somestate ** 2
-		# else:
-		# 	return 0
+		reward = self.hvacBuilding.DetermineReward()
+		return reward
